4b24afe2dc684a3cafc28246c2d38582b441af2b
fuochung0905 <6251071033@st.utc2.edu.vn>
2023-09-20 14:34:36 +0700

    begin

---
+++ CNPM-Ecommerce-UTC2/CNPM-Ecommerce-UTC2/Controllers/CartController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CNPM_ktxUtc2Store.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+        public async Task<IActionResult> AddItem(int productId, int quantity=1,int redirect=0)
+        {
+            var cartCount = await _cartService.AddItem(productId, quantity);
+            if(redirect == 0) { 
+                return Ok(cartCount);
+            }
+            return RedirectToAction("GetUserCart");
+        }
+
+        public async Task<IActionResult> Remove(int productId)
+        {
+            var cartCount= await _cartService.RemoveItem(productId);
+
+            return RedirectToAction("GetUserCart");
+        }
+        public async Task<IActionResult> GetUserCart()
+        {
+            var cart= await _cartService.GetUserCart();
+            return View(cart);
+        }
+        public async Task<IActionResult> GetTotalItemInCart()
+        {
+            int cartItem=await _cartService.GetCartItemCount();
+            return Ok(cartItem);
+        }
+
+    }
+}
